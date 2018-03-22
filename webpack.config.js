19d2493c180317175275bbf922c2f6f2c8b5cd06
hamilton <hamilton.ulmer@gmail.com>
2018-03-22 14:37:44 -0700

    reverting webpack trick

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ let CSS_PATH_STRING
 module.exports = (env) => {
 
   return {
-    entry: `${APP_DIR}/src.js`,
+    entry: `${APP_DIR}/ply.js`,
     mode: 'development',
     output: {
       path: BUILD_DIR,
